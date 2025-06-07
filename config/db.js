48dd7981f7e77fc958b3d4f06bb342caf2d51fbe
Pallab Jyoti Bora <pallabjyotibora75@gmail.com>
2025-06-07 12:36:12 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -23,8 +23,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
       maxPoolSize: 10, // Adjust based on your needs
       serverSelectionTimeoutMS: 5000
     });
